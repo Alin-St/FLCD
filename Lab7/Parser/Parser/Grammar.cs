297e5e593827de71844ce94d1f42cc32b4b928a4
Alin Stanciu <alinstanciu2011@yahoo.com>
2024-01-19 11:58:20 +0200

    lab7 init

---
+++ Lab7/Parser/Parser/Grammar.cs
@@ -0,0 +1,139 @@
+﻿namespace Parser;
+
+class Grammar
+{
+    List<string> N { get; set; }  // non-terminals
+    List<string> E { get; set; }  // terminals
+    string S { get; set; }        // starting symbol/axiom
+    Dictionary<string, List<List<string>>> P { get; set; }  // finite set of productions
+
+    public Grammar()
+    {
+        N = new List<string>();
+        E = new List<string>();
+        S = "";
+        P = new Dictionary<string, List<List<string>>>();
+    }
+
+    public void Rebuild()
+    {
+        N.Clear();
+        E.Clear();
+        S = "";
+        P.Clear();
+    }
+
+    static List<string> ProcessLine(string line, string delimiter = " ")
+    {
+        var elements = new List<string>(line.Trim().Trim('{', '}').Split(delimiter));
+
+        if (elements.Count > 1)
+        {
+            elements[0] += delimiter;
+            var combined = string.Join("", elements.GetRange(0, 2));
+            elements.RemoveRange(0, 2);
+            elements.Insert(0, combined);
+        }
+
+        return elements.ConvertAll(element => element.Trim());
+    }
+
+    public void ReadFromFile(string fileName)
+    {
+        Rebuild();
+        using var file = new StreamReader(fileName);
+        string line = file.ReadLine()!;
+        N = ProcessLine(line.Split('=')[1], ", ");
+
+        line = file.ReadLine()!;
+        E = ProcessLine(line[(line.IndexOf('=') + 1)..].Trim(), ", ");
+
+        line = file.ReadLine()!;
+        S = ProcessLine(line.Split('=')[1], ", ")[0];
+
+        while ((line = file.ReadLine()!) != null && line.Trim() != "" && !line.Contains(" -> ")) ;
+
+        while (line != null)
+        {
+            if (line.Contains(" -> "))
+            {
+                string[] parts = line.Split(" -> ");
+                string source = parts[0].Trim();
+                string productions = parts[1];
+                foreach (var production in productions.Split('|'))
+                {
+                    var productionArr = production.Trim().Split();
+                    if (P.ContainsKey(source))
+                    {
+                        P[source].Add(productionArr.ToList());
+                    }
+                    else
+                    {
+                        P[source] = new List<List<string>> { productionArr.ToList() };
+                    }
+                }
+            }
+
+            line = file.ReadLine()!;
+        }
+    }
+
+    public bool CheckCfg()
+    {
+        bool hasStartingSymbol = false;
+
+        foreach (string key in P.Keys)
+        {
+            if (key == S)
+            {
+                hasStartingSymbol = true;
+            }
+
+            if (!N[0].Split().Contains(key))
+            {
+                return false;
+            }
+        }
+
+        if (!hasStartingSymbol)
+        {
+            return false;
+        }
+
+        foreach (List<List<string>> production in P.Values)
+        {
+            foreach (List<string> rhs in production)
+            {
+                foreach (string value in rhs)
+                {
+                    if (!N[0].Split().Contains(value) && !E[0].Split().Contains(value))
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public List<string> NonTerminals { get => N; }
+    public List<string> Terminals { get => E; }
+    public string StartSymbol { get => S; }
+    public Dictionary<string, List<List<string>>> Productions { get => P; }
+
+    public List<List<string>> GetProductionsForNonTerminal(string nt)
+    {
+        var found = P.TryGetValue(nt, out var productions);
+        return found && productions is not null ? productions : new();
+    }
+
+    public override string ToString()
+    {
+        string result = "N = " + Util.StrListToString(N) + "\n";
+        result += "E = " + Util.StrListToString(E) + "\n";
+        result += "S = " + S + "\n";
+        result += "P = " + Util.DictStrListListStrToString(P) + "\n";
+        return result;
+    }
+}
---
+++ Lab7/Parser/Parser/Parser.cs
@@ -0,0 +1,184 @@
+﻿namespace Parser;
+
+class Parser
+{
+    private readonly Grammar grammar;
+    private readonly List<string> sequence;
+    private readonly List<object> working;
+    private List<string> input;
+    private string state;
+    private int index;
+
+    public Parser(Grammar grammar, string sequenceFile)
+    {
+        this.grammar = grammar;
+        this.sequence = ReadSequence(sequenceFile);
+        this.working = new List<object>();
+        this.input = new List<string> { this.grammar.StartSymbol };
+        this.state = "q";
+        this.index = 0;
+    }
+
+    private static List<string> ReadSequence(string seqFile)
+    {
+        var seq = new List<string>();
+        using (var reader = new StreamReader(seqFile))
+        {
+            string line;
+            while ((line = reader.ReadLine()!) != null)
+            {
+                seq.Add(line.Trim());
+            }
+        }
+        return seq;
+    }
+
+    public void GetSituation()
+    {
+        Console.WriteLine($"({state}, {index}, {Util.ObjListToString(working)}, {Util.StrListToString(input)})");
+    }
+
+    public void Expand()
+    {
+        Console.WriteLine("|--- expand");
+        var nonTerminal = input.First(); // non_terminal = "S"
+        input.RemoveAt(0);
+        working.Add(Tuple.Create(nonTerminal, 0)); // working = [("S", 0)]
+
+        var newProduction = grammar.GetProductionsForNonTerminal(nonTerminal)[0]; // new_production = ["a", "S", "b", "S"]
+        this.input = newProduction.Concat(this.input).ToList();
+    }
+
+    public void Advance()
+    {
+        Console.WriteLine("|--- advance");
+        var item = this.input.First();
+        this.input.RemoveAt(0);
+        this.working.Add(item); // working = [("S", 0), "a"]
+        this.index++; // (q, i + 1, w, s)
+    }
+
+    public void MomentaryInsuccess()
+    {
+        Console.WriteLine("|--- momentary insuccess");
+        this.state = "b"; // (b, i, w, s)
+    }
+
+    public void Back()
+    {
+        Console.WriteLine("|--- back");
+        var item = this.working.Last(); // item = "a"
+        this.working.RemoveAt(this.working.Count - 1);
+        this.input.Insert(0, (string)item); // self.input = ["S", "a", "S", "b", "S"]
+        this.index--;
+    }
+
+    public void Success()
+    {
+        Console.WriteLine("|--- success");
+        this.state = "f";
+        var msg = $"(f, {this.index}, {Util.ObjListToString(working)}, {Util.StrListToString(input)})\n=> sequence is syntactically correct\n";
+        Console.WriteLine(msg);
+    }
+
+    public void AnotherTry()
+    {
+        Console.WriteLine("|--- another try");
+        if (working.Any())
+        {
+            var lastNt = (Tuple<string, int>)this.working.Last();
+            working.RemoveAt(working.Count - 1);
+            var nt = lastNt.Item1;
+            var productionNr = lastNt.Item2;
+
+            var productions = grammar.GetProductionsForNonTerminal(nt);
+
+            if (productionNr + 1 < productions.Count)
+            {
+                state = "q";
+
+                var newTuple = Tuple.Create(nt, productionNr + 1);
+                working.Add(newTuple);
+
+                var lenLastProduction = productions[productionNr].Count;
+                input = input.Skip(lenLastProduction).ToList();
+                var newProduction = productions[productionNr + 1];
+                input = newProduction.Concat(input).ToList();
+            }
+            else
+            {
+                var lenLastProduction = productions[productionNr].Count;
+                input = input.Skip(lenLastProduction).ToList();
+                if (input.Count != 0)
+                {
+                    input.Insert(0, nt);
+                }
+            }
+        }
+        else
+        {
+            state = "e";
+        }
+    }
+
+    public void Error()
+    {
+        Console.WriteLine("|--- error");
+        this.state = "e";
+        var msg = $"(e, {this.index}, {string.Join(", ", this.working)}, {string.Join(", ", this.input)})\nNo more input to look at!";
+        Console.WriteLine(msg);
+    }
+
+    public void Run()
+    {
+        while (this.state != "f" && this.state != "e")
+        {
+            this.GetSituation();
+            if (this.state == "q")
+            {
+                if (this.input.Count == 0 && this.index == this.sequence.Count)
+                {
+                    this.Success();
+                }
+                else
+                {
+                    if (this.input.First() is string first && this.grammar.NonTerminals[0].Split(" ").Contains(first))
+                    {
+                        this.Expand();
+                    }
+                    else
+                    {
+                        if (this.index < this.sequence.Count && this.input.First().Equals(this.sequence[this.index]))
+                        {
+                            this.Advance();
+                        }
+                        else
+                        {
+                            this.MomentaryInsuccess();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (this.state == "b")
+                {
+                    if (this.working.Any() && this.grammar.Terminals[0].Split(" ").Contains(working.Last()))
+                    {
+                        this.Back();
+                    }
+                    else
+                    {
+                        this.AnotherTry();
+                    }
+                }
+            }
+        }
+
+        if (this.state == "e")
+        {
+            this.GetSituation();
+            this.Error();
+        }
+    }
+}
